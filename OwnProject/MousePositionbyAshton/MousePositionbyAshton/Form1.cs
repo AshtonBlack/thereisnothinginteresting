951facece554c209c0887fb8435fcf3fb43f0f16
Ashton Black <stormywarrior67@gmail.com>
2020-01-13 21:47:21 +0300

    add mouse prog

---
+++ OwnProject/MousePositionbyAshton/MousePositionbyAshton/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace MousePositionbyAshton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Timer t = new Timer();
+            t.Interval = 100;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();            
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            label1.Text = Cursor.Position.X.ToString();
+            label2.Text = Cursor.Position.Y.ToString();
+        }
+
+    }
+}
