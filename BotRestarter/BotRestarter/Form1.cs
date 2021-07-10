27b3bc387e2b2424cf16cb211fed10b576493f34
Ashton Black <stormywarrior67@gmail.com>
2021-07-10 22:36:09 +0300

    mend

---
+++ BotRestarter/BotRestarter/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BotRestarter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Opacity = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread.Sleep(20000);
+        
+            Process.Start(@"C:\Bot\Bot.v.0.1\WindowsFormsApp1\bin\Debug\WindowsFormsApp1.exe");
+
+            Application.Exit();
+        }
+    }
+}
