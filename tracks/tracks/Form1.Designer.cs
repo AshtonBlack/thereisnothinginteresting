f1eb5f06ea95c5eed3c786c7ebbde78f2fbfdb88
Ashton <stormywarrior67@gmail.com>
2022-04-10 10:27:55 +0300

    mend

---
+++ tracks/tracks/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace tracks
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(64, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(161, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(280, 23);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 203);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 301);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(297, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Save new track";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ tracks/tracks/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tracks
+{
+    public partial class Form1 : Form
+    {
+        public string[,] trackpicturetoname { get; set; }
+        public int tracklength { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            TrackPictureToNameTable();
+        }
+
+        public void TrackPictureToNameTable()
+        {
+            string commonpath = @"C:\projects\bot\thereisnothinginteresting\Track";
+            string path = @"\info.txt";
+            tracklength = 0;
+            using (StreamReader sr = new StreamReader(commonpath + comboBox1.Text + path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null && line != " " && line != "")
+                {
+                    tracklength++;
+                }
+                sr.Close();
+            }
+            trackpicturetoname = new string[tracklength, 2];
+
+            using (StreamReader sr = new StreamReader(commonpath + comboBox1.Text + path, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < tracklength; i++)
+                {
+                    string theline = sr.ReadLine();
+                    trackpicturetoname[i, 0] = Transform3(theline, 1);
+                    trackpicturetoname[i, 1] = Transform3(theline, 2);
+                }
+                sr.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string path = @"C:\projects\bot\thereisnothinginteresting\";
+            string ending = ".jpg";
+            if (File.Exists(path + "Track" + comboBox1.Text + "\\" + textBox1.Text + ending))
+            {
+                pictureBox1.Image = Image.FromFile(path + "Track" + comboBox1.Text + "\\" + textBox1.Text + ending);
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
+
+            label1.Text = path + "Track" + comboBox1.Text + "\\" + textBox1.Text + ending;
+            comboBox2.Text = "unknown";
+
+            for (int i = 0; i < tracklength; i++)
+            {
+                if (trackpicturetoname[i, 0] == textBox1.Text)
+                {
+                    comboBox2.Text = trackpicturetoname[i, 1];
+                    break;
+                }
+            }
+        }
+
+        public string Transform3(string t, int wordN)
+        {
+            string forreturn;
+            string a = t.Trim();
+            char[] word = a.ToCharArray();
+
+            int wordBlength = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != ' ')
+                {
+                    wordBlength++;
+                }
+                else break;
+            }
+            char[] wordB = new char[wordBlength];
+            for (int i = 0; i < wordB.Length; i++)
+            {
+                wordB[i] = word[i];
+            }
+
+            char[] wordC = new char[word.Length - wordBlength - 1];
+            for (int i = 0; i < wordC.Length; i++)
+            {
+                wordC[i] = word[i + wordBlength + 1];
+            }
+
+            if (wordN == 1)
+            {
+                forreturn = new string(wordB);
+            }
+            else
+            {
+                forreturn = new string(wordC);
+            }
+            return forreturn;
+        }
+
+        public void TrackPictureToNameTableAdd()
+        {
+            string commonpath = @"C:\projects\bot\thereisnothinginteresting\Track";
+            string path = @"\info.txt";
+            string thetrack = comboBox2.Text;
+            using (StreamWriter sw = new StreamWriter(commonpath + comboBox1.Text + path, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(textBox1.Text + " " + thetrack);
+                sw.Close();
+            }
+            TrackPictureToNameTable();
+            textBox1.Text = (Convert.ToInt32(textBox1.Text) + 1).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TrackPictureToNameTableAdd();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Items.AddRange(ReadTracks().ToArray());
+        }
+
+        public List<String> ReadTracks()
+        {
+            List<String> tracks = new List<String>();
+            using (StreamReader sr = new StreamReader(@"C:\projects\bot\1.txt", System.Text.Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null && line != " " && line != "")
+                {
+                    tracks.Add(line);
+                }
+                sr.Close();
+            }
+            tracks.Sort();
+            return tracks;
+        }
+
+        public void SaveNewTrack(string text)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(@"C:\projects\bot\1.txt", true, System.Text.Encoding.UTF8))//true для дописывания 
+                {
+                    //sw.WriteLine();
+                    sw.WriteLine(text);
+                    sw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            SaveNewTrack(textBox2.Text);
+            comboBox2.Items.AddRange(ReadTracks().ToArray());
+        }
+    }
+}
