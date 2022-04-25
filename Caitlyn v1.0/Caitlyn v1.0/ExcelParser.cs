846cc9c4d3409fc74952e7b882e51b7839eae048
Ashton Black <stormywarrior67@gmail.com>
2022-04-25 10:25:23 +0300

    mend

---
+++ Caitlyn v1.0/Caitlyn v1.0/ExcelParser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Caitlyn_v1._0
+{
+    class ExcelParcer
+    {
+        static int rows;
+        static int columns;
+        public static List<CarForExcel> Parse(string excelFilePath)
+        {
+            return HandleTable(Parser(excelFilePath));
+        }
+        static object[,] Parser(string excelFilePath)
+        {
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(excelFilePath);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1-й лист
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
+            rows = lastCell.Row;
+            columns = lastCell.Column;
+            var arrData = (object[,])ObjWorkSheet.Range["A1:Z" + lastCell.Row].Value;
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            ObjWorkExcel.Quit(); // выйти из Excel
+            GC.Collect(); // убрать за собой
+            KillExcel();
+
+            return arrData;
+        }
+        static void KillExcel()
+        {
+            Process[] processes = Process.GetProcessesByName("Microsoft Excel");
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+        static List<CarForExcel> HandleTable(object[,] fulltable)
+        {
+            List<CarForExcel> cartable = new List<CarForExcel>();
+            for (int i = 0; i < rows; i++)
+            {
+                CarForExcel car = new CarForExcel();
+                car.country = fulltable[i + 1, 4].ToString();
+                car.manufacturer = fulltable[i + 1, 5].ToString();
+                car.model = fulltable[i + 1, 6].ToString();
+                car.year = fulltable[i + 1, 7].ToString();
+                car.rarity = fulltable[i + 1, 2].ToString();
+                car.tires = fulltable[i + 1, 15].ToString();
+                car.rq = fulltable[i + 1, 3].ToString();
+                car.drive = fulltable[i + 1, 13].ToString();
+                car.fuel = fulltable[i + 1, 23].ToString();
+                car.body = fulltable[i + 1, 24].ToString();
+                car.seats = fulltable[i + 1, 25].ToString();
+                try
+                {
+                    car.tags = fulltable[i + 1, 26].ToString();
+                }
+                catch(Exception ex)
+                {
+
+                }
+                car.clearance = fulltable[i + 1, 18].ToString();
+                car.acceleration = fulltable[i + 1, 9].ToString();
+                car.speed = fulltable[i + 1, 8].ToString();
+                car.grip = fulltable[i + 1, 12].ToString();
+                cartable.Add(car);
+            }
+
+            return cartable;
+        }
+    }
+}
---
+++ Caitlyn v1.0/Caitlyn v1.0/TrackInfo.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Caitlyn_v1._0
+{
+    public class TrackInfo
+    {
+        string trackDirectory = @"C:\Bot\Track";
+        string groundDirectory = @"C:\Bot\Ground";
+        string weatherDirectory = @"C:\Bot\Weather";
+        public int[] Tracks()
+        {
+            int n;
+            bool flag;
+            Rectangle[] a = { PointsAndRectangles.Track1, PointsAndRectangles.Track2, PointsAndRectangles.Track3, PointsAndRectangles.Track4, PointsAndRectangles.Track5 };
+            int[] a1 = new int[5];            
+
+            for (int i = 0; i < 5; i++)
+            {
+                flag = true;
+                MasterOfPictures.TrackCapture(a[i], ("Track" + (i + 1) + "\\test"));
+                n = 0;
+                for (int i1 = 1; i1 < 100; i1++)
+                {
+                    if (File.Exists(trackDirectory + (i + 1) + "\\" + i1 + ".jpg"))
+                    {
+                        n = i1;
+                    }
+                    else break;
+                }
+
+                for (int i2 = 1; i2 < (n + 1); i2++)
+                {
+                    if (MasterOfPictures.VerifyBW(("Track" + (i + 1) + "\\" + i2), ("Track" + (i + 1) + "\\test"), 120))
+                    {
+                        a1[i] = i2;
+                        File.Delete(trackDirectory + (i + 1) + "\\test.jpg");
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag == true)
+                {
+                    a1[i] = 0;
+                    NotePad.DoLog("Добавляю новый трэк");
+                    File.Move(trackDirectory + (i + 1) + "\\test.jpg", trackDirectory + (i + 1) + "\\" + (n + 1) + ".jpg");
+                }
+            }
+
+            return a1;
+        }
+
+        public int[] Grounds()
+        {
+            Rectangle[] b = { PointsAndRectangles.Ground1, PointsAndRectangles.Ground2, PointsAndRectangles.Ground3, PointsAndRectangles.Ground4, PointsAndRectangles.Ground5 };
+            int n;
+            bool flag;
+            int[] b1 = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                flag = true;
+                MasterOfPictures.BW2Capture(b[i], ("Ground" + (i + 1) + "\\test"));
+                n = 0;
+                for (int i1 = 1; i1 < 100; i1++)
+                {
+                    if (File.Exists(groundDirectory + (i + 1) + "\\" + i1 + ".jpg"))
+                    {
+                        n = i1;
+                    }
+                    else break;
+                }
+
+                for (int i2 = 1; i2 < (n + 1); i2++)
+                {
+                    if (MasterOfPictures.VerifyBW(("Ground" + (i + 1) + "\\" + i2), ("Ground" + (i + 1) + "\\test"), 150))
+                    {
+                        b1[i] = i2;
+                        File.Delete(groundDirectory + (i + 1) + "\\test.jpg");
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag == true)
+                {
+                    b1[i] = 0;
+                    NotePad.DoLog("Добавляю новое покрытие");
+                    File.Move(groundDirectory + (i + 1) + "\\test.jpg", groundDirectory + (i + 1) + "\\" + (n + 1) + ".jpg");
+                }
+            }
+
+            return b1;
+        }
+
+        public int[] Weathers()
+        {
+            Rectangle[] c = { PointsAndRectangles.Weather1, PointsAndRectangles.Weather2, PointsAndRectangles.Weather3, PointsAndRectangles.Weather4, PointsAndRectangles.Weather5 };
+            int n;
+            bool flag;
+            int[] c1 = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                flag = true;
+                MasterOfPictures.BW2Capture(c[i], ("Weather" + (i + 1) + "\\test"));
+                n = 0;
+                for (int i1 = 1; i1 < 10; i1++)
+                {
+                    if (File.Exists(weatherDirectory + (i + 1) + "\\" + i1 + ".jpg"))
+                    {
+                        n = i1;
+                    }
+                    else break;
+                }
+
+                for (int i2 = 1; i2 < (n + 1); i2++)
+                {
+                    if (MasterOfPictures.VerifyBW(("Weather" + (i + 1) + "\\" + i2), ("Weather" + (i + 1) + "\\test"), 30))
+                    {
+                        c1[i] = i2;
+                        File.Delete(weatherDirectory + (i + 1) + "\\test.jpg");
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag == true)
+                {
+                    c1[i] = 0;
+                    NotePad.DoLog("Добавляю новую погоду");
+                    try
+                    {
+                        File.Move(weatherDirectory + (i + 1) + "\\test.jpg", weatherDirectory + (i + 1) + "\\" + (n + 1) + ".jpg");
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+
+            return c1;
+        }
+
+        public string[,] TrackPackage(int[] trackName, int[] coverage, int[] weather)
+        {
+            string[] a2 = IdentifyTracks(trackName);
+            string[] b2 = IdentifyGround(coverage);
+            string[] c2 = IdentifyWeather(weather);
+            string[,] d = new string[3, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                d[0, i] = a2[i];
+                d[1, i] = b2[i];
+                d[2, i] = c2[i];
+
+                NotePad.DoLog((i + 1) + " Трэк: " + d[0, i] + " " + d[1, i] + " " + d[2, i]);
+            }
+
+            return d;
+        }
+
+        public string[] IdentifyGround(int[] b1)
+        {
+            string[] grounds = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                string name = "unknown";
+                int length = NotePad.GetInfoFileLength(groundDirectory + (i + 1) + "\\info.txt");
+                string[,] theTable = NotePad.ReadInfoFromTXT(groundDirectory + (i + 1) + "\\info.txt");
+                for (int l = 0; l < length; l++)
+                {
+                    if (b1[i] == Convert.ToInt32(theTable[l, 0]))
+                    {
+                        name = theTable[l, 1];
+                        grounds[i] = name;
+                        break;
+                    }
+                }
+                if (name == "unknown")
+                {
+                    NotePad.DoErrorLog("Неизвестное покрытие позиция " + (i + 1));
+                    grounds[i] = "Неизвестная покрытие";
+                }
+            }
+
+            bool asphalt = false;
+            bool mud = false;
+
+            foreach (string x in grounds)
+            {
+                if (x == "Асфальт")
+                {
+                    asphalt = true;
+                }
+                else
+                {
+                    mud = true;
+                }
+            }
+
+            if (asphalt)
+            {
+                Condition.coverage = "Асфальт";
+                if (mud) Condition.coverage = "Смешанное";
+            }
+            else Condition.coverage = "Бездорожье";
+            NotePad.LastCoverage(Condition.coverage);
+            return grounds;
+        }
+
+        public string[] IdentifyWeather(int[] c1)
+        {
+            string[] weathers = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                string name = "unknown";
+                int length = NotePad.GetInfoFileLength(weatherDirectory + (i + 1) + "\\info.txt");
+                string[,] theTable = NotePad.ReadInfoFromTXT(weatherDirectory + (i + 1) + "\\info.txt");
+                for (int l = 0; l < length; l++)
+                {
+                    if (c1[i] == Convert.ToInt32(theTable[l, 0]))
+                    {
+                        name = theTable[l, 1];
+                        weathers[i] = name;
+                        break;
+                    }
+                }
+                if (name == "unknown")
+                {
+                    NotePad.DoErrorLog("Неизвестная погода позиция " + (i + 1));
+                    weathers[i] = "Неизвестная погода";
+                }
+            }
+
+            bool dry = false;
+            bool wet = false;
+
+            foreach (string x in weathers)
+            {
+                if (x == "Дождь")
+                {
+                    wet = true;
+                }
+
+                if (x == "Солнечно")
+                {
+                    dry = true;
+                }
+            }
+
+            Condition.weather = "ясно";
+            if (wet)
+            {
+                if (dry) Condition.weather = "с прояснением";
+                else Condition.weather = "дождь";
+            }
+            NotePad.LastWeather(Condition.weather);
+
+            return weathers;
+        }
+
+        public string[] IdentifyTracks(int[] a1)
+        {
+            string[] tracks = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                string name = "unknown";
+                int length = NotePad.GetInfoFileLength(trackDirectory + (i + 1) + "\\info.txt");
+                string[,] theTable = NotePad.ReadInfoFromTXT(trackDirectory + (i + 1) + "\\info.txt");
+                for (int l = 0; l < length; l++)
+                {
+                    if (a1[i] == Convert.ToInt32(theTable[l, 0]))
+                    {
+                        name = theTable[l, 1];
+                        tracks[i] = name;
+                        break;
+                    }
+                }
+                if (name == "unknown")
+                {
+                    NotePad.DoErrorLog("Неизвестная трасса позиция " + (i + 1));
+                    tracks[i] = "Неизвестная трасса";
+                }
+            }
+            return tracks;
+        }
+    }
+}
