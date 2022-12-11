0afa1e7671701aad4644c8788711793c62482b45
unknown <stormywarrior67@gmail.com>
2022-12-11 03:01:29 +0300

    mend

--- Caitlyn v1.0/Caitlyn v1.0/Algorithms.cs
+++ Caitlyn v1.0/Caitlyn v1.0/Algorithms.cs
@@ -236,37 +236,12 @@ namespace Caitlyn_v1._0
             }
             switch (trackInfo.track)
             {
-                case "Длинная трасса у океана":
-                    x *= 2;
-                    x -= Convert.ToDouble(car.acceleration) * 20;
-                    x += Convert.ToInt16(car.speed);
-                    break;
-                case "Короткая трасса у океана":
-                    x *= 2;
-                    x -= Convert.ToDouble(car.acceleration) * 20;
-                    x += Convert.ToInt16(car.speed) / 2;
-                    break;
-                case "Магистраль у океана":
-                    x *= 3;
-                    x += Convert.ToInt16(car.speed);
-                    x -= Convert.ToDouble(car.acceleration) * 5;
-                    x += Convert.ToInt16(car.grip);
-                    break;
-                case "Парковка у океана":
-                case "Пляжный слалом у океана":
-                    x *= 4;
-                    x -= Convert.ToDouble(car.acceleration) * 3;
-                    x += Convert.ToInt16(car.grip) * 2;
-                    x -= Convert.ToInt16(car.weight);
-                    break;
                 case "Городские улицы у океана":
                 case "Улица ср":
                 case "Улица мал":
-                    x *= 3;
                     if(car.clearance != "low") x += 1000;
                     break;
                 case "Подъем на холм":
-                    x *= 3;
                     x -= Convert.ToDouble(car.acceleration) * 5;
                     if (trackInfo.ground != "Асфальт")
                     {
@@ -283,7 +258,6 @@ namespace Caitlyn_v1._0
                     }
                     break;
                 case "Трасса для мотокросса":
-                    x *= 4;
                     if (car.drive == "4wd") x += 200;
                     switch (car.clearance)
                     {
@@ -296,7 +270,6 @@ namespace Caitlyn_v1._0
                     }
                     break;
                 case "50-150":
-                    x *= 1;
                     x -= Convert.ToDouble(car.acceleration) * 20;
                     x += Convert.ToInt16(car.speed) * 5;
                     if (Convert.ToInt16(car.speed) < 150)
@@ -305,7 +278,6 @@ namespace Caitlyn_v1._0
                     }
                     break;
                 case "75-125":
-                    x *= 1;
                     x -= Convert.ToDouble(car.acceleration) * 20;
                     x += Convert.ToInt16(car.speed) * 4;
                     if (Convert.ToInt16(car.speed) < 130)
@@ -313,55 +285,30 @@ namespace Caitlyn_v1._0
                         x -= 2000;
                     }
                     break;
+                case "0-60":
                 case "0-100":
-                    x *= 3;
-                    x -= Convert.ToDouble(car.acceleration) * 10;
-                    x += Convert.ToInt16(car.speed) * 2;
-                    if (Convert.ToInt16(car.speed) < 105)
-                    {
-                        x -= 2000;
-                    }
-                    break;
                 case "0-100-0":
-                    x *= 3.5;
+                case "1/4":
                     x -= Convert.ToDouble(car.acceleration) * 10;
                     x += Convert.ToInt16(car.speed) * 2;
-                    if (Convert.ToInt16(car.speed) < 105)
+                    if (Convert.ToInt16(car.speed) < 110)
                     {
                         x -= 2000;
                     }
                     break;
                 case "1":
-                    x *= 1.5;
                     x -= Convert.ToDouble(car.acceleration) * 10;
                     x += Convert.ToInt16(car.speed) * 3;
                     break;
+                case "Длинная трасса у океана":
+                case "Токио трасса":
+                case "Трасса набережная":
+                case "Короткая трасса у океана":
                 case "1/2":
-                    x *= 2.5;
                     x -= Convert.ToDouble(car.acceleration) * 5;
                     x += Convert.ToInt16(car.speed);
                     break;
-                case "1/4":
-                    x *= 3;
-                    x -= Convert.ToDouble(car.acceleration) * 20;
-                    x += Convert.ToInt16(car.speed) / 2;
-                    break;
-                case "0-60":
-                    x *= 4;
-                    x -= Convert.ToDouble(car.acceleration) * 20;
-                    if (Convert.ToInt16(car.speed) < 60)
-                    {
-                        x -= 2000;
-                    }
-                    break;
-                case "Токио трасса":
-                case "Трасса набережная":
-                    x *= 2;
-                    x -= Convert.ToDouble(car.acceleration) * 10;
-                    x += Convert.ToInt16(car.speed);
-                    break;
                 case "Тестовый круг":
-                    x *= 1;
                     x += Convert.ToInt16(car.speed) * 5;
                     break;
                 case "Нюрбург 1":
@@ -369,13 +316,7 @@ namespace Caitlyn_v1._0
                 case "Нюрбург 3":
                 case "Нюрбург 4":
                 case "Нюрбург 5":
-                    x *= 2.5;
                     x += Convert.ToInt16(car.speed) * 3;
-                    break;
-                case "Токио петля":
-                    x *= 2.5;
-                    x += Convert.ToInt16(car.speed) * 2;
-                    x -= Convert.ToDouble(car.acceleration) * 5;
                     break;
                 case "Замерзшее озеро":
                     if (car.drive == "4wd") x += 100;
@@ -398,26 +339,7 @@ namespace Caitlyn_v1._0
                             break;
                     }
                     break;
-                case "Горы серпантин":
-                    x *= 4;
-                    if (trackInfo.ground != "Асфальт" || trackInfo.weather != "Солнечно")
-                    {
-                        if (car.drive == "4wd") x += 50;
-                    }
-                    break;
-                case "Горы извилистая дорога":
-                    x *= 4;
-                    x -= Convert.ToDouble(car.acceleration) * 3;
-                    x += Convert.ToInt16(car.grip) * 2;
-                    break;
-                case "Горы дорога с уклоном":
-                    x *= 3;
-                    x += Convert.ToInt16(car.speed);
-                    x -= Convert.ToDouble(car.acceleration) * 3;
-                    x += Convert.ToInt16(car.grip);
-                    break;
-                case "Горы подъем на холм":
-                    x *= 3;                    
+                case "Горы подъем на холм":   
                     x -= Convert.ToDouble(car.acceleration) * 5;
                     x += Convert.ToInt16(car.grip)/2;
                     if (trackInfo.ground != "Асфальт")
@@ -434,13 +356,7 @@ namespace Caitlyn_v1._0
                         }
                     }
                     break;
-                case "Горная экспедиция":
-                    x *= 2.5;
-                    x += Convert.ToInt16(car.speed)*2;
-                    x += Convert.ToInt16(car.grip) / 4;
-                    break;
                 case "Извилистая дорога":
-                    x *= 4;
                     if (trackInfo.ground != "Асфальт")
                     {
                         if (car.drive == "4wd") x += 100;
@@ -455,42 +371,34 @@ namespace Caitlyn_v1._0
                         }
                     }
                     break;
+                case "Горы дорога с уклоном":
+                case "Горная экспедиция":
+                case "Магистраль у океана":
                 case "Токио мостик":
-                case "Обзор":
-                case "Серпантин":
-                case "Монако серпантин":
+                case "Обзор":                
                 case "Токио мост":
                 case "Токио съезд":
                 case "Монако городские":
                 case "Каньон экспедиция":
                 case "Быстрая трасса":
                 case "Highway":
-                    x *= 2.5;
                     x -= Convert.ToDouble(car.acceleration) * 3;
                     x += Convert.ToInt16(car.grip);
                     x += Convert.ToInt16(car.speed);
                     break;
+                case "Токио петля":
+                case "Серпантин":
+                case "Монако серпантин":
                 case "Монако длинные городские улицы":
                 case "Извилистая трасса":
-                    x *= 3;
-                    x -= Convert.ToDouble(car.acceleration) * 2;
-                    x += Convert.ToInt16(car.grip) * 2;
-                    break;
                 case "Лесная дорога":
                 case "Лесная переправа":
                 case "Каньон грунтовая дорога":
                 case "Каньон крутой холм":                    
                 case "Ралли-кросс мал":
                 case "Ралли-кросс ср":
-                    x *= 3;
                     x -= Convert.ToDouble(car.acceleration) * 2;
                     x += Convert.ToInt16(car.grip) * 2;
-                    break;
-                case "Монако узкие улицы":
-                    x *= 2.5;
-                    x -= Convert.ToDouble(car.acceleration);
-                    x += Convert.ToInt16(car.grip) * 3;
-                    x -= Convert.ToInt16(car.weight)/10;
                     break;
                 case "Монако тест на перегрузки":
                 case "Токио тест на перегрузки":
@@ -500,12 +408,15 @@ namespace Caitlyn_v1._0
                 case "Горы слалом":
                 case "Слалом":
                 case "Перегрузка":
-                    x *= 4.5;
                     x += Convert.ToInt16(car.grip) * 5;
                     x -= Convert.ToInt16(car.weight)/10;
                     break;
+                case "Горы серпантин":
+                case "Горы извилистая дорога":
+                case "Монако узкие улицы":
+                case "Парковка у океана":
+                case "Пляжный слалом у океана":
                 case "Трасса для картинга":
-                    x *= 4.5;
                     x -= Convert.ToDouble(car.acceleration);
                     x += Convert.ToInt16(car.grip) * 3;
                     x -= Convert.ToInt16(car.weight)/10;
