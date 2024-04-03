dc9135cdc4d0ed24fe46f3b06937c7d8f08ab158
unknown <stormywarrior67@gmail.com>
2024-04-04 00:42:39 +0300

    mend

--- Caitlyn v1.0/Caitlyn v1.0/SolveDoubleDevice.cs
+++ Caitlyn v1.0/Caitlyn v1.0/SolveDoubleDevice.cs
@@ -6,7 +6,7 @@ namespace Caitlyn_v1._0
     {
         public override bool SolveTheIssue()
         {
-            if (VisualCheck.Check(PointsAndRectangles.doubleDevice, "DoubleDevice"))
+            if (VisualCheck.Check(PointsAndRectangles.doubleDevice, "DoubleDevice", 800))
             {
                 NotePad.DoLog("Master is in game");
                 Thread.Sleep(20 * 60 * 1000);
